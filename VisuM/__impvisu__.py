f681153ffd0a2402e987dfc6163d315df078e396
Guillaume Barthe <guillaume.barthe@epitech.eu>
2019-07-12 16:40:33 +0200

    GBARTHE: add .gitignore

--- VisuM/__impvisu__.py
+++ VisuM/__impvisu__.py
@@ -1,14 +1,9 @@
 import sys
 
 ### Module to load saved witch data data ###
-sys.path.append('../AdditionnalModules/SaveAndLoadModule/')
-import convertLoadedData
-import save
+sys.path.append('..')
+import SaveAndLoadModule.convertLoadedData
+import SaveAndLoadModule.save
 
-### Module with usefull functions to dict manipulations ###
-sys.path.append('../AdditionnalModules/DictOpModule/')
-import OpDict
-
-### Module with os usefull functions ###
-sys.path.append('../AdditionnalModules/UsefullFunc/')
-import utilsfunc
+import AdditionnalModules.DictOpModule.OpDict
+import AdditionnalModules.UsefullFunc.utilsfunc
